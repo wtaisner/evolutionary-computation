e4c223d69d18bda0954f6505ced6a62ad5ebf1d0
annprzy <anneprzybylowska@gmail.com>
2022-10-13 20:50:28 +0200

    implement greedy cycle, experiment

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,5 +1,6 @@
 import time
 from abc import ABC, abstractmethod
+from copy import deepcopy
 
 import numpy as np
 
@@ -17,10 +18,31 @@ class TSP(ABC):
         coords = nodes[:, :2]
         self.dist_matrix = np.linalg.norm(coords[:, None, :] - coords[None, :, :], axis=-1).round()
 
+    def run_experiment(self):
+        """
+        performs function run_algorithm starting from each node
+        :return: (min, max, avg, edges, start) - min, max , avg value of objective function, additionally for minimal
+        value of objective returns edges and starting node
+        """
+        minv, maxv, avgv = np.inf, -np.inf, 0
+        min_edges, starting_node = None, None
+        for i in range(self.n):
+            objective, edges = self.run_algorithm(i)
+
+            avgv += objective
+            if objective > maxv:
+                maxv = objective
+            if objective < minv:
+                minv = objective
+                min_edges = edges
+                starting_node = i
+
+        return minv, maxv, avgv / self.n, min_edges, starting_node
+
     @abstractmethod
     def run_algorithm(self, starting_node: int):
         """
-        Abstract method for impllementation of given algorithm that solves TSP problem
+        Abstract method for implementation of given algorithm that solves TSP problem
         :param starting_node: index of the starting node
         :return: tuple in form (objective, path), where objective represents the value of objective function for given
         solutions and path is numpy array containing consecutive nodes in the Hamiltonian cycle
@@ -37,11 +59,10 @@ class RandomTSP(TSP):
         perc_50 = int(np.ceil(self.n * 0.5))
         nodes = np.arange(0, self.n)
         nodes = np.delete(nodes, starting_node)
-        path.extend(list(np.random.choice(nodes, perc_50 - 1)))
-        pairs = np.concatenate((path, np.roll(path, -1)), axis=0).T
-        distances = np.sum(self.dist_matrix[pairs])
+        path.extend(list(np.random.choice(nodes, perc_50 - 1, replace=False)))
+        distances = np.sum(self.dist_matrix[path, np.roll(path, -1)])
         costs = np.sum(self.costs[path])
-        return int(distances + costs), path
+        return int(distances + costs), np.concatenate(([path], np.roll([path], -1)), axis=0).T
 
 
 class NearestNeighbourTSP(TSP):
@@ -53,18 +74,18 @@ class NearestNeighbourTSP(TSP):
         path = [starting_node]
         distances = 0
         costs = self.costs[starting_node]
+        cost = deepcopy(self.costs)
+        dist_matrix = deepcopy(self.dist_matrix)
         for _ in range(perc_50 - 1):
             i = path[-1]
-            dist = self.dist_matrix[i, :]
-            dist[path] = None
-            cost = self.costs
-            cost[path] = None
+            dist = dist_matrix[i, :]
+            dist[path], cost[path] = None, None
             new_node = np.nanargmin(dist + cost)
             path.append(new_node)
-            distances += self.dist_matrix[i, new_node]
+            distances += dist_matrix[i, new_node]
             costs += self.costs[new_node]
         distances += self.dist_matrix[path[-1], path[0]]
-        return int(distances + costs), path
+        return int(distances + costs), np.concatenate(([path], np.roll([path], -1)), axis=0).T
 
 
 class GreedyCycleTSP(TSP):
@@ -72,11 +93,37 @@ class GreedyCycleTSP(TSP):
     A class implementing greedy cycle method for solving the TSP problem
     """
     def run_algorithm(self, starting_node: int):
-        pass
+        perc_50 = int(np.ceil(self.n * 0.5))
+        dist_matrix = deepcopy(self.dist_matrix)
+        dist = dist_matrix[starting_node, :]
+        cost = deepcopy(self.costs)
+        dist[[starting_node]], cost[[starting_node]] = None, None
+        new_node = np.nanargmin(dist + cost)
+        edges = [[starting_node, new_node], [new_node, starting_node]]
+        nodes = [starting_node, new_node]
+        costs = self.costs[starting_node] + self.costs[new_node]
+        dists = 2 * self.dist_matrix[starting_node, new_node]
+
+        for _ in range(perc_50 - 2):
+            dist_matrix[nodes, nodes] = None
+            cost[nodes] = None
+            i = np.argmin([np.nanmin(dist_matrix[i, :] + dist_matrix[j, :] + cost - self.dist_matrix[i, j]) for i, j in edges])
+            i_edge = edges[i]
+            dist_i, dist_j = dist_matrix[i_edge[0], :], dist_matrix[i_edge[1], :]
+            new_node = np.nanargmin(dist_i + dist_j + cost)
+            edges.pop(i)
+            edges.append([i_edge[0], new_node])
+            edges.append([new_node, i_edge[1]])
+            nodes.append(new_node)
+            costs += self.costs[new_node]
+            dists += self.dist_matrix[i_edge[0], new_node] + self.dist_matrix[i_edge[1], new_node] - self.dist_matrix[i_edge[0], i_edge[1]]
+
+        return int(costs + dists), edges
 
 
 if __name__ == '__main__':
-    nnTSP = NearestNeighbourTSP('../data/TSPA.csv')
+    nnTSP = GreedyCycleTSP('../data/TSPA.csv')
+    #nnTSP = NearestNeighbourTSP('../data/TSPA.csv')
     start = time.time()
     print(nnTSP.run_algorithm(0))
     print(time.time() - start)
--- src/visualization.py
+++ src/visualization.py
@@ -3,18 +3,16 @@ import networkx as nx
 import numpy as np
 
 
-def plot_result(nodes: np.ndarray, path: np.array, objective: int):
+def plot_result(nodes: np.ndarray, edges: np.array, objective: int):
     """
     plots obtained result
     :param nodes: numpy array containing (x,y) coordinates and costs for given nodes
-    :param path: numpy array containing consecutive nodes to visit
+    :param edges: numpy array containing edges that form the path
     :param objective: value of objective function for the path found
     :return: plot
     """
     G = nx.Graph()
     G.add_nodes_from(np.arange(len(nodes)))
-    path = [path]
-    edges = np.concatenate((path, np.roll(path, -1)), axis=0).T
     G.add_edges_from(edges)
 
     pos = nodes[:, :2]
@@ -25,5 +23,4 @@ def plot_result(nodes: np.ndarray, path: np.array, objective: int):
     nc = nx.draw_networkx_nodes(G, pos, node_color=cost, label=None, node_size=20, cmap='Blues')
     plt.colorbar(nc)
     plt.text(-5, -20, f'objective: {objective}')
-    # plt.axis('off')
     plt.show()
